820e3856d3df15ebadc5f2de086d79fe37311314
linyongxin <172754827@qq.com>
2017-06-29 16:58:30 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div ><App></App> <div className="app"><p>haahah</p> </div> </div>, document.getElementById('root'));
+registerServiceWorker();
